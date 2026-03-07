3eff1d39fb8810eca3811d08a888e804a4eb522e
Mike <86262609+michaelmandic@users.noreply.github.com>
2026-03-07 15:00:09 +0100

    updated rss feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  site: 'https://michaelmandic.com'
+
+  site: 'https://michaelmandic.com',
+  integrations: [sitemap()]
 });
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,14 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdowIt from 'markdown-it';
+
+const parser = new MarkdowIt();
 
 export async function GET(context) {
     const blog = await getCollection('blog');
     return rss({
-        title: "Michael Mandic's Blog",
+        title: "Michael Mandic",
         description: "SWE and student at the University of Belgrade",
         site: context.site,
         items: blog.map((post) => ({
@@ -14,6 +18,10 @@ export async function GET(context) {
             // Compute RSS link from post `id`
             // This example assumes all posts are rendered as `/blog/[id]` routes
             link: `/blog/${post.id}/`,
+            content: sanitizeHtml(parser.render(post.body), {
+                allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+            }),
         })),
+        stylesheet: "/rss/styles.xsl",
     });
 }
